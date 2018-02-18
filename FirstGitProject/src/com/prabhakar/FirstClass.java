c98425563bcdab034e1dcc931c053192417d422b
Prabhakar Singh <er.prabhakarsingh09@gmail.com>
2018-02-18 14:08:33 +0530

    m1 method in First class is added

--- FirstGitProject/src/com/prabhakar/FirstClass.java
+++ FirstGitProject/src/com/prabhakar/FirstClass.java
@@ -5,5 +5,10 @@ public class FirstClass {
 	public void m1(){
 		System.out.println("This is m1 method of first class");
 	}
+	
+	public void m2(){
+		System.out.println("This is m2 method of first class");
+	}
+	
 
 }
